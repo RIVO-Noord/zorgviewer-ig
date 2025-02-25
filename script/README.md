28e0b3f8cc39525d564e30373cf490d9be3e98d3
Michael van der Zel <michael.vanderzel@gmail.com>
2025-02-25 21:48:18 +0100

    add rows from examples

--- script/README.md
+++ script/README.md
@@ -1,2 +1,7 @@
 @> apt update
+@> apt install curl
+https://www.linode.com/docs/guides/install-nodejs-on-ubuntu-22-04/
+@> curl -sL https://deb.nodesource.com/setup_23.x | bash -
 @> apt install nodejs
+@> npm install --save fhirpath
+! needs newer version (>=14) of node
--- script/updateviewpuml.js
+++ script/updateviewpuml.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const path = require('path');
+const fhirpath = require('fhirpath');
+const fhirpath_stu3_model = require('fhirpath/fhir-context/stu3');
 
 const viewDefPath = "../input/images/";
 const pumlPath = "../input/images-source/";
@@ -19,6 +21,35 @@ fs.readdirSync(viewDefPath).forEach(file => {
         // add column names for select
         if (viewDef.select[0].column) {
             doColumns(viewDef.select[0].column, puml);
+
+            // Add column examples based on examples
+            const match2 = viewDef.select[0].forEach.match("resourceType='(.+)'");
+            if (match2) {
+                puml.push("| |");
+                const resourceType = match2[1];
+                const examplesPath = "../input/examples";
+                fs.readdirSync(examplesPath).forEach(file => {
+                    if (!file.endsWith(".json")) return;
+                    const example_filePath = path.join(examplesPath, file);
+                    const example = JSON.parse(fs.readFileSync(example_filePath, 'utf8'));
+                    if (example.resourceType == resourceType) {
+                        const values = viewDef.select[0].column.map(column => {
+                            var result;
+                            try { result = fhirpath.evaluate(example, `${resourceType}.${column.path}`); }
+                            catch { }
+                            var value = "";
+                            if (result && result.length > 0) {
+                                value = result[0].replace(/\r?\n/g, "\\n ");
+                                if (value.length > 80) value = `${value.substring(0,80)}...`;
+                            }
+                            return value;
+                        });
+                        // overwrite bron 1ste kolom met filename
+                        values[0] = file.substring(resourceType.length+1, file.length-5);
+                        puml.push(`| ${values.join(' | ')} |`);
+                    }
+                });
+            }
         }
         if (viewDef.select[0].unionAll) {
             // assume first columns is the final list of columns
@@ -38,29 +69,11 @@ fs.readdirSync(viewDefPath).forEach(file => {
 function doColumns(columns, puml) {
     const columnNames = columns.map(column => ` ${column.name} `);
     puml.push(`|=${columnNames.join('|=')}|`);
-    // TODO: add column examples based on examples
-    // TODO: figure out why full path breaks PlantUML
-    const columnPaths = columns.map(column => ` ${column.path.split('.')[0] || ""}...`);
-    puml.push(`|${columnPaths.join('|')}|`);
-    const columnTypes = columns.map(column => ` ${column.type || ""} `);
-    puml.push(`|${columnTypes.join('|')}|`);
-    const columnZib = columns.map(column => column.tag ? ` ${column.tag[0].value.split('/').join('\\n /')} ` : "");
-    puml.push(`|${columnZib.join('|')}|`);
+    // TODO: figure out why full path breaks PlantUML; for now split
+    const columnPaths = columns.map(column => column.path.split('.')[0] || "");
+    puml.push(`| ${columnPaths.join('... | ')} |`);
+    const columnTypes = columns.map(column => column.type || "");
+    puml.push(`| ${columnTypes.join(' | ')} |`);
+    const columnZib = columns.map(column => column.tag ? column.tag[0].value.split('/').join('\\n /') : "");
+    puml.push(`| ${columnZib.join(' | ')} |`);
 }
-
-/*
-@startsalt medicatie
-skinparam Backgroundcolor white
-{+
-    {}
-    == <&eyedropper> Medicatie gebruik
-    {}
-    {T#
-    **Bron** | **Datum** | **Medicatie** | **Toedieningsweg** | **Instructies** | **Categorie**
-    <&medical-cross> UMCG | 15-feb-2022 | nitroprusside 1mg/ml spuit voor pomp | Intraveneus | continu 1 microg/min intraveneus toedienen | Community 
-    . | . | generate rows from examples
-    }
-    {}
-}
-@endsalt
-*/
