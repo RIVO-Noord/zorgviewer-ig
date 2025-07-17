ada15bb555524ff23feab3bf8c3a7b47586d6843
Michael van der Zel <michael.vanderzel@gmail.com>
2025-07-17 08:31:45 +0200

    do examples for union

--- input/includes/ViewDefinition-Medicatiegegevens-ui.md
+++ input/includes/ViewDefinition-Medicatiegegevens-ui.md
@@ -11,6 +11,46 @@
 <th>Toedieningsweg</th>
 <th>Stop Type</th>
 </tr>
+<tr><td>+</td>
+<td>Chipsoft</td>
+<td>15-8-2016</td>
+<td>14-9-2016</td>
+<td>METOCLOPRAMIDE ZETPIL 10MG</td>
+<td> 1x elke 1 dag, indien nodig (Zo nodig)<br/>Aanvullende instructie 1: Pas op met alcohol<br/>Kan het reactievermogen verminderen<br/>Niet langer dan 5 dagen achtereen gebr..  &#9432;</td>
+<td>RECTAAL</td>
+<td>Definitief</td>
+</tr><tr><td></td><td colspan=7>
+<b>Afspraakdatum</b><br/>30-9-2022<br/>
+<b>Voorschrijver</b><br/>Peter van Pulver<br/>
+<b>Type</b><br/>voorgeschreven<br/>
+</td></tr>
+<tr><td>+</td>
+<td>Epic</td>
+<td>27-11-2020</td>
+<td></td>
+<td>metoPROLOL 50mg tablet</td>
+<td>1x per dag 50 mg innemen.</td>
+<td>Oraal</td>
+<td></td>
+</tr><tr><td></td><td colspan=7>
+<b>Afspraakdatum</b><br/>27-11-2020<br/>
+<b>Medicatie Vorm</b><br/>tablet<br/>
+<b>Voorschrijver</b><br/>M Crop<br/>
+<b>Type</b><br/>voorgeschreven<br/>
+</td></tr>
+<tr><td>+</td>
+<td>Nexus</td>
+<td>3-10-2023</td>
+<td></td>
+<td>GLYCEROFOSFORZ. INFCONC 1MMOL/ML(FOSFAAT) AMP 20ML</td>
+<td> 1x elke 1 dag om 08:00:00 &#9432;</td>
+<td>INTRAVENEUS</td>
+<td></td>
+</tr><tr><td></td><td colspan=7>
+<b>Afspraakdatum</b><br/>3-10-2023<br/>
+<b>Voorschrijver</b><br/>Aalders, JCA (CAR)<br/>
+<b>Type</b><br/>voorgeschreven<br/>
+</td></tr>
 <tr><th>&gt;&lt;</th>
 <th>Bron</th>
 <th>Start</th>
@@ -19,5 +59,16 @@
 <th>Dosering & instructies</th>
 <th>Toedieningsweg</th>
 </tr>
+<tr><td>+</td>
+<td>Epic</td>
+<td></td>
+<td></td>
+<td>simvastatine 20mg tablet</td>
+<td>innemen. s Avonds innemen; Bij dit middel GEEN grapefruit(sap) gebruiken</td>
+<td>Oraal</td>
+</tr><tr><td></td><td colspan=6>
+<b>Registratiedatum</b><br/>15-3-2018<br/>
+<b>Type</b><br/>huidig<br/>
+</td></tr>
 </tbody>
 </table>
--- script/updateviewmd.js
+++ script/updateviewmd.js
@@ -64,88 +64,21 @@ fs.readdirSync(viewDefPath).forEach(file => {
          ];
 
         // add column names for select
-        const columnPaths = [];
         if (viewDef.select[0].column) {
             doColumns(viewDef.select[0].column, md_ui, md_def);
-            viewDef.select[0].column.forEach(column => columnPaths.push(column.path));
-
-            // Add column examples based on examples
-            const match2 = viewDef.select[0].forEach.match("resourceType='(.+)'");
-            if (match2) {
-                const resourceType = match2[1];
-                const examplesPath = "../input/examples";
-                fs.readdirSync(examplesPath).forEach(file => {
-                    if (!file.endsWith(".json")) return;
-                    const example_filePath = path.join(examplesPath, file);
-                    const example = JSON.parse(fs.readFileSync(example_filePath, 'utf8'));
-
-                    // generate if dosageInstruction but no text; only for MedicationRequest/Statements
-                    if (example.dosageInstruction && !example.dosageInstruction[0].text) {
-                        // var text = dosageToString(example.dosageInstruction[0]);
-                        var text = dosageToStringGemini(example.dosageInstruction[0]);
-                        if (text.includes("undefined")) {
-                            console.error("Some expected dosage parts undefined?", JSON.stringify(example.dosageInstruction));
-                        }
-                        example.dosageInstruction[0].text = text + ' &#9432;';
-                    }
-
-                    // only include in table when where clause applies
-                    const match3 = viewDef.select[0].forEach.match(".where\((.+)\)");
-                    if (match3) {
-                        var result;
-                        try { result = fhirpath.evaluate(example, match3[1]); }
-                        catch { }
-                        if (result[0]) {
-                            const values = viewDef.select[0].column.map(column => {
-                                var result;
-                                try { result = fhirpath.evaluate(example, `${column.path}`, null, fhirpath_stu3_model, { userInvocationTable }); }
-                                catch (err) { console.error(column.name, err.message, column.path); }
-                                var value = "";
-                                if (result && result.length > 0) {
-                                    if (column.type == "date" || column.type == "dateTime") {
-                                        const date = new Date(result[0]);
-                                        value = date.toLocaleDateString('nl-NL'); // + ' ' + date.toLocaleTimeString('nl-NL';
-                                    }
-                                    else {
-                                        if (value.length > 80) value = `${value.substring(0,80)}...`;    
-                                        value = result[0].replace(/\r?\n/g, "<br/>");
-                                    }
-                                }
-                                return value;
-                            });
-                            // column 0 is altijd bron en set bron obv filename
-                            values[0] = file.substring(file.indexOf('-')+1, file.length-5);
-                            md_ui.push("<tr><td>+</td>");
-                            // add column values
-                            viewDef.select[0].column.forEach((column,idx) => {
-                                if (column.name.charAt(0) != '+') {
-                                    md_ui.push(`<td>${values[idx]}</td>`);
-                                }
-                            });
-                            // add uitklap values; only show if there is a value
-                            const colcount = viewDef.select[0].column.filter(column => column.name.charAt(0) != '+').length;
-                            md_ui.push(`</tr><tr><td></td><td colspan=${colcount}>`);
-                            viewDef.select[0].column.forEach((column,idx) => {
-                                if (column.name.charAt(0) == '+' && values[idx] != "") {
-                                    md_ui.push(`<b>${column.name.slice(1)}</b><br/>${values[idx]}<br/>`);
-                                }
-                            });
-                            md_ui.push("</td></tr>");
-                        }
-                    }
-                });
-            }
+            doExampleRows(viewDef.select[0], md_ui);
         }
         if (viewDef.select[0].unionAll) {
-            // assume first columns is the final list of columns
+            // assume first unions columns is the final list of columns
             viewDef.select[0].unionAll.forEach(union => {
                 const match = union.forEach.match("resourceType='(.+)'");
+                console.log(match);
                 if (match) {
                     const resourceType = match[1];
                     md_def.push(`<tr><td colspan=5><i>${resourceType}</i></td></tr>`);
                 }
                 doColumns(union.column, md_ui, md_def);
-                union.column.forEach(column => columnPaths.push(column.path));
+                doExampleRows(union, md_ui);
             });
         }
         md_def.push("</tbody>",
@@ -206,4 +139,73 @@ function doColumn(column, md) {
     md.push(`<td><code>${column.type && column.type.split('/').join("</code> of <code>")}</code></td>`)
     md.push(`<td>${column.tag?column.tag[0].value:"<i>nvt</i>"}</td>`)
     md.push(`<td>${column.description||""}</td>`);
+}
+
+function doExampleRows(select, md_ui) {
+    // Add column examples based on examples
+    const match2 = select.forEach.match("resourceType='(.+)'");
+    if (match2) {
+        const resourceType = match2[1];
+        const examplesPath = "../input/examples";
+        fs.readdirSync(examplesPath).forEach(file => {
+            if (!file.endsWith(".json")) return;
+            const example_filePath = path.join(examplesPath, file);
+            const example = JSON.parse(fs.readFileSync(example_filePath, 'utf8'));
+
+            // generate if dosageInstruction but no text; only for MedicationRequest/Statements
+            if (example.dosageInstruction && !example.dosageInstruction[0].text) {
+                // var text = dosageToString(example.dosageInstruction[0]);
+                var text = dosageToStringGemini(example.dosageInstruction[0]);
+                if (text.includes("undefined")) {
+                    console.error("Some expected dosage parts undefined?", JSON.stringify(example.dosageInstruction));
+                }
+                example.dosageInstruction[0].text = text + ' &#9432;';
+            }
+
+            // only include in table when where clause applies
+            const match3 = select.forEach.match(".where\((.+)\)");
+            if (match3) {
+                var result;
+                try { result = fhirpath.evaluate(example, match3[1]); }
+                catch { }
+                if (result[0]) {
+                    const values = select.column.map(column => {
+                        var result;
+                        try { result = fhirpath.evaluate(example, `${column.path}`, null, fhirpath_stu3_model, { userInvocationTable }); }
+                        catch (err) { console.error(column.name, err.message, column.path); }
+                        var value = "";
+                        if (result && result.length > 0) {
+                            if (column.type == "date" || column.type == "dateTime") {
+                                const date = new Date(result[0]);
+                                value = date.toLocaleDateString('nl-NL'); // + ' ' + date.toLocaleTimeString('nl-NL';
+                            }
+                            else {
+                                if (value.length > 80) value = `${value.substring(0,80)}...`;    
+                                value = result[0].replace(/\r?\n/g, "<br/>");
+                            }
+                        }
+                        return value;
+                    });
+                    // column 0 is altijd bron en set bron obv filename
+                    values[0] = file.substring(file.indexOf('-')+1, file.length-5);
+                    md_ui.push("<tr><td>+</td>");
+                    // add column values
+                    select.column.forEach((column,idx) => {
+                        if (column.name.charAt(0) != '+') {
+                            md_ui.push(`<td>${values[idx]}</td>`);
+                        }
+                    });
+                    // add uitklap values; only show if there is a value
+                    const colcount = select.column.filter(column => column.name.charAt(0) != '+').length;
+                    md_ui.push(`</tr><tr><td></td><td colspan=${colcount}>`);
+                    select.column.forEach((column,idx) => {
+                        if (column.name.charAt(0) == '+' && values[idx] != "") {
+                            md_ui.push(`<b>${column.name.slice(1)}</b><br/>${values[idx]}<br/>`);
+                        }
+                    });
+                    md_ui.push("</td></tr>");
+                }
+            }
+        });
+    }
 }
